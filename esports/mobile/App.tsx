bd3ffacb748f2366f8b2ab3c69d9bb6080ac1ae3
lucasceifa <lucassilvatrindade@hotmail.com>
2022-09-27 10:17:48 -0300

    first commit

---
+++ esports/mobile/App.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef } from "react";
+import { StatusBar } from "react-native";
+import * as Notifications from 'expo-notifications';
+
+import { useFonts, Inter_400Regular, Inter_900Black, Inter_600SemiBold, Inter_700Bold } from "@expo-google-fonts/inter";
+
+import { Routes } from "./src/routes";
+import { Loading } from "./src/components/Loading";
+import { Background } from "./src/components/Background";
+import './src/services/notificationsConfigs'
+import { getPushNotificationToken } from './src/services/getNotificationPush'
+import { Subscription } from 'expo-modules-core'
+
+const getNotificationListener = useRef< Subscription >();
+const responseNotificationListener = useRef< Subscription >();
+
+useEffect(() => {
+  getPushNotificationToken();
+})
+useEffect(() => {
+  getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+    console.log(notification)
+  })
+  responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener( response => {
+    console.log(response)
+  })
+
+  return () => {
+    if(getNotificationListener.current && responseNotificationListener.current){
+      Notifications.removeNotificationSubscription(getNotificationListener.current)
+      Notifications.removeNotificationSubscription(responseNotificationListener.current)
+    }
+  }
+})
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_900Black,
+    Inter_600SemiBold,
+    Inter_700Bold
+  });
+  return (
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <Routes /> : <Loading/>}
+    </Background>
+  );
+}
